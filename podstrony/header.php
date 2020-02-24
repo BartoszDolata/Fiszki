9833589ece7c7ba17ec1c7c4431ce96a5fc10b30
Bartosz Dolata <54848431+dolata-bartosz@users.noreply.github.com>
2020-02-24 21:18:04 +0100

    link

--- podstrony/header.php
+++ podstrony/header.php
@@ -1,5 +1,5 @@
 
-<a href="../index.php">Fiszkopedia</a>
+<a href="./sets.php">Fiszkopedia</a>
 <div class="burger"><i class="fas fa-bars"></i></div>
 <div class="burger"><i class="fas fa-angle-double-left see"></i></div>
 <div class="sign"><a href="../scripts/logout.php"><i class="fas fa-sign-in-alt"></i></a></div>
